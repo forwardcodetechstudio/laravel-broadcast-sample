be31c742713fda3c8b974d29287ee9b5c7d60be4
beingshaif01 <saif@forwardcode.in>
2023-05-04 13:05:02 +0530

    Route Created for Authorization callback function of notifications.{userId} channel

--- routes/channels.php
+++ routes/channels.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Broadcast;
 
 /*
@@ -15,4 +16,11 @@ use Illuminate\Support\Facades\Broadcast;
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
+});
+
+/**
+ * Route Created for Authorization callback function of notifications.{userId} channel
+ */
+Broadcast::channel('notifications.{userId}', function (User $user, int $userId) {
+    return $user->id == $userId;
 });
